eecfc7cef9bc17059c5094f9d5ab3319fc03297e
Aditya Ambati <ambati@stanford.edu>
2018-12-03 11:08:22 -0800

    Create RANDOMLY_SELECT_INDIVIDUALS_FROM_PLINKFAM.py

---
+++ RANDOMLY_SELECT_INDIVIDUALS_FROM_PLINKFAM.py
@@ -0,0 +1,33 @@
+import random
+import argparse
+import datetime
+dttime=datetime.datetime.now().strftime ("%Y%m%d")
+parser = argparse.ArgumentParser(description='A script to randomize sample selection for a big GWAS cohort')
+parser.add_argument('-LIST', required=True, help='list of IDS one per line file that contains the IDs')
+parser.add_argument('-NO', required=True, help='Number of individuals to randomly select')
+### parse the arguments
+args=parser.parse_args()
+famfile = args.FAM
+num_ind = int(args.NO)
+
+
+def main(famfile, num_ind):
+	fam_dic = {}
+	n = 0
+	with open(famfile) as infile:
+		for line in infile:
+			if 'FAM' not in line:
+				n += 1
+				fam_dic[n] = line
+		print('PARSED IDS FROM {} INDIVIDUALS'.format(n))
+
+	if num_ind <= n:
+		raise ValueError('Number of Individuals input is much greater than N in the dataset')
+	#num_ind = 5000
+	rand_sample = random.sample(fam_dic.keys(), num_ind)
+	out_rand_sample=[fam_dic.get(i) for i in rand_sample if i in fam_dic]
+	make_outf = open(famfile.replace('.fam', '_RANDOM_'+str(num_ind)+'.txt'), 'w')
+	for item in out_rand_sample:make_outf.write(item)
+	make_outf.close()
+
+if __name__ == '__main__':main(famfile=famfile, num_ind=num_ind)
